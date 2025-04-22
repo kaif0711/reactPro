8b870b7d79fa28aeb53b414d101b18de3c571968
kaif mansuri0 <kaifmansuri80076@gmail.com>
2025-04-21 23:18:06 -0700

    Updates

---
+++ precache-manifest.75557c3bdc64a69e7c507539b86af740.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34df754ac962ea6bdfaa7ce358e2d20d",
+    "url": "/reactPro/index.html"
+  },
+  {
+    "revision": "b85959edd3400b99ebd7",
+    "url": "/reactPro/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e079a2d0e495ab787664",
+    "url": "/reactPro/static/js/2.020b9f20.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/reactPro/static/js/2.020b9f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cc9d638bf604d280f1",
+    "url": "/reactPro/static/js/3.49433e1c.chunk.js"
+  },
+  {
+    "revision": "b85959edd3400b99ebd7",
+    "url": "/reactPro/static/js/main.461e3436.chunk.js"
+  },
+  {
+    "revision": "51cdc05af2361c639a02",
+    "url": "/reactPro/static/js/runtime-main.382a73f2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactPro/precache-manifest.78a273aef5e240bb3f324cccfcb55a21.js"
+  "/reactPro/precache-manifest.75557c3bdc64a69e7c507539b86af740.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactPro/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
